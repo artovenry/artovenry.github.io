b9070c61bd7c2ae7c1d4146b40ed9978c43df5b1
artovenry <artovenry@users.noreply.github.com>
2022-08-02 09:07:10 +0000

    deploy: b51b96efe1458ff63300514db01dce32579738f1

--- _nuxt/static/1659430902/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fasia\u002Fmount-everest","\u002Fsouth america\u002Faconcagua","\u002Fantarctic\u002Fvinson-massif","\u002Fafrica\u002Fmount-kilimanjaro","\u002Fnorth america\u002Fdenali","\u002Faustralia\u002Fmount-kosciuszko","\u002Feurope\u002Fmount-blanc"]})
---
+++ _nuxt/static/1659431229/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Feurope\u002Fmount-blanc","\u002Fafrica\u002Fmount-kilimanjaro","\u002Fantarctic\u002Fvinson-massif","\u002Faustralia\u002Fmount-kosciuszko","\u002Fnorth america\u002Fdenali","\u002Fasia\u002Fmount-everest","\u002Fsouth america\u002Faconcagua"]})
