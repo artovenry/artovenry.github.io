f26fdf51d8a2804ad23e06922b709d42f1dfc680
artovenry <artovenry@users.noreply.github.com>
2022-08-02 09:01:43 +0000

    deploy: d3ec97d09f5b21776c791ceef7cdbf774a8d4281

--- _nuxt/static/1659430654/europe/mount-blanc/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659430654",layout:"default",error:a,serverRendered:true,routePath:"\u002Feurope\u002Fmount-blanc",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
